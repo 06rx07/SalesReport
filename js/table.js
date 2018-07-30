ec044f941b69dba8ecc4f132f8339d11bb8aa0ef
Rachel Xi <rachelxi@Rachels-MacBook-Pro.local>
2018-07-30 23:36:30 +1200

    Add row span

--- js/table.js
+++ js/table.js
@@ -21,21 +21,52 @@ const getTable = {
         }
     },
     createTable: function (salesData) {
-        const productsCat = (Array.isArray(salesData)) ? salesData.length : 0;
+        const processedData = this.separateDataByProduct(salesData);
         let table = null;
-        if (productsCat) {
+        if (processedData.length) {
             table = document.createElement('table');
-            table.appendChild(this.appendHeaders());
-            for (let i = 0; i < productsCat; i++) {
-                table.appendChild(this.appendRow(salesData[i]));
+            table.appendChild(this.appendHeaders(processedData[0].category));
+            for (let i = 0; i < processedData.length; i++) {
+                for (let j = 0; j < processedData[i].data.length; j++) {
+                    const rowSpan = (j === 0) ? processedData[i].data.length : undefined;
+                    const row = this.appendRow(processedData[i].data[j], processedData[i].category, rowSpan);
+                    table.appendChild(row);
+                }
             }
         }
         return table;
     },
-    appendHeaders: function () {
+    getGroupByCategory: function (salesData) {
+        if (salesData.length > 1) {
+            const refRegion = salesData[0].region;
+            if (salesData.findIndex(data => data.region !== refRegion) === -1) {
+                return 'region';
+            }
+        }
+        return 'product';
+    },
+    separateDataByProduct: function (salesData) {
+        const refCategory = this.getGroupByCategory(salesData);
+        const result = [];
+        let resultEle = {};
+        for (let data of salesData) {
+            if (data[refCategory] !== resultEle.key) {
+                if (resultEle.key) {
+                    result.push(resultEle);
+                }
+                resultEle = { category: refCategory, key: data[refCategory], data: [data] };
+            } else {
+                resultEle.data.push(data);
+            }
+        }
+        result.push(resultEle);
+        return result;
+    },
+    appendHeaders: function (firstCol) {
         const row = document.createElement('tr');
-        row.appendChild(this.getHeader('Product'));
-        row.appendChild(this.getHeader('Region'));
+        row.appendChild(this.getHeader(firstCol));
+        const secondCol = firstCol === 'product' ? 'region': 'product';
+        row.appendChild(this.getHeader(secondCol));
         row.appendChild(this.getHeader('Jan'));
         row.appendChild(this.getHeader('Feb'));
         row.appendChild(this.getHeader('Mar'));
@@ -56,19 +87,25 @@ const getTable = {
         header.appendChild(headerText);
         return header;
     },
-    appendRow: function (rowData) {
+    appendRow: function (rowData, spanKey, spanRow) {
         const row = document.createElement('tr');
-        row.appendChild(this.getRowElement(rowData['product']));
-        row.appendChild(this.getRowElement(rowData['region']));
+        if (typeof spanRow !== 'undefined') {
+            row.appendChild(this.getRowElement(rowData[spanKey], spanRow));
+        }
+        const appendKey = (spanKey === 'product') ? 'region' : 'product';
+        row.appendChild(this.getRowElement(rowData[appendKey]));
         for (let i = 0; i < rowData['sale'].length; i++) {
             row.appendChild(this.getRowElement(rowData['sale'][i]));
         }
         return row;
     },
-    getRowElement: function (text) {
+    getRowElement: function (text, rowSpan) {
         const row = document.createElement('td');
         const rowText = document.createTextNode(text);
         row.appendChild(rowText);
+        if (typeof rowSpan !== 'undefined') {
+            row.rowSpan = rowSpan;
+        }
         return row;
     },
 };
