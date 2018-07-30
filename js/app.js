a5ceca241899e29938e05c3dfa40c944b45df961
Rachel Xi <rachelxi@Rachels-MacBook-Pro.local>
2018-07-30 21:45:34 +1200

    Add select all

--- js/app.js
+++ js/app.js
@@ -1,36 +1,75 @@
-const regionSelect = document.querySelector('#region-select');
-const productSelect = document.querySelector('#product-select');
+const regionSelect = document.querySelectorAll('input[name="region"]');
+const productSelect = document.querySelectorAll('input[name="product"]');
+const regionAll = document.querySelector('input[name="region-all"]');
+const productAll = document.querySelector('input[name="product-all"]');
+
+const init = function () {
+    getTable.getTableBySelect(action.getValue(regionSelect), action.getValue(productSelect));
+    for (let node of regionSelect) {
+        node.addEventListener('change', action.selectRegion);
+    }
+    for (let node of productSelect) {
+        node.addEventListener('change', action.selectProduct);
+    }
+    regionAll.addEventListener('change', action.selectAll);
+    productAll.addEventListener('change', action.selectAll);
+};
 
 const action = {
-    init: function () {
-        getTable.getTableBySelect(this.getValue(regionSelect), this.getValue(productSelect));
+    getValue: function (parentNodes) {
+        return Array.from(parentNodes)
+            .filter(node => node.checked)
+            .map(checkbox => checkbox.value);
+    },
+    keepChecked: function (target) {
+        target.checked = true;
     },
     selectRegion: function (event) {
-        if (event.target.localName === 'input') {
-            const regionValue = this.getValue(regionSelect);
-            if (regionValue.length) {
-                getTable.getTableBySelect(regionValue, this.getValue(productSelect));
-            } else {
-                this.keepLastSelection(event.target);
-            }
+        const regionValue = action.getValue(regionSelect);
+        if (regionValue.length) {
+            (regionValue.length < 3) ? action.checkSelectAllRegion(false) :
+                action.checkSelectAllRegion(true);
+            getTable.getTableBySelect(regionValue, action.getValue(productSelect));
+        } else if (event) {
+            action.keepChecked(event.target);
         }
     },
     selectProduct: function (event) {
-        if (event.target.localName === 'input') {
-            const productValue = this.getValue(productSelect);
-            if (productValue.length) {
-                getTable.getTableBySelect(this.getValue(regionSelect), productValue);
-            } else {
-                this.keepLastSelection(event.target);
-            }
+        const productValue = action.getValue(productSelect);
+        if (productValue.length) {
+            (productValue.length < 3) ? action.checkSelectAllProduct(false) :
+                action.checkSelectAllProduct(true);
+            getTable.getTableBySelect(action.getValue(regionSelect), productValue);
+        } else if (event) {
+            action.keepChecked(event.target);
         }
     },
-    getValue: function (parentNode) {
-        return Array.from(parentNode.childNodes)
-            .filter(node => node.localName === 'input' && node.checked)
-            .map(checkbox => checkbox.value);
+    selectAll: function (event) {
+        switch (event.target.name) {
+            case 'region-all':
+                action.selectAllRegions(); break;
+            case 'product-all':
+                action.selectAllProducts(); break;
+        }
     },
-    keepLastSelection: function (target) {
-        target.checked = true;
+    selectAllRegions: function () {
+        if (regionAll.checked) {
+            const nodes = Array.from(regionSelect)
+                .forEach(select => select.checked = true);
+            action.selectRegion();
+        }
+    },
+    selectAllProducts: function () {
+        if (productAll.checked) {
+            const nodes = Array.from(productSelect)
+                .forEach(select => select.checked = true);
+            action.selectProduct();
+        }
+    },
+    checkSelectAllRegion: function (status) {
+        regionAll.checked = status;
+    },
+    checkSelectAllProduct: function (status) {
+        productAll.checked = status;
     }
 };
