db4137e249af9b7a409d2f8236f1eab5bd42aa47
Rachel Xi <rachelxi@wifi-staff-172-24-9-147.net.auckland.ac.nz>
2018-08-11 15:03:31 +1200

    Update practice push state

--- route-practice/push-state.js
+++ route-practice/push-state.js
@@ -6,26 +6,17 @@ Array.from(buttons).forEach(btn => btn.addEventListener('click', btnCLickHandler
 window.addEventListener('popstate', showButtonContent);
 
 function getState() {
-    const hash = (history.state || '').substring(1).split('&&');
-    const result = {};
-    for (let i = 0; i < stateKey.length; i++) {
-        result[stateKey[i]] = (hash[i]) ? hash[i].split('=')[1] : '';
-    }
-    return result;
+    return history.state || {};
 }
 
 function showButtonContent() {
-    const hashResult = getState();
-    abcDiv.innerHTML = hashResult[stateKey[0]];
-    defDiv.innerHTML = hashResult[stateKey[1]];
+    const stateResult = getState();
+    abcDiv.innerHTML = stateResult[stateKey[0]] || '';
+    defDiv.innerHTML = stateResult[stateKey[1]] || '';
 }
 
 function saveState(result) {
-    let hashString = '';
-    for (let key in result) {
-        hashString += key + '=' + result[key] + '&&';
-    }
-    history.pushState(hashString.substring(0, hashString.length - 2), null);
+    history.pushState(result, null);
     showButtonContent();
 }
 
